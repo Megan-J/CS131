f68ea44df4bbfd92319eac99c06c3fb276576121
Megan <megansp24@sjsu.pic2.isv.ihost.com>
2024-04-23 03:29:11 -0400

    fixed formatting

--- MiniProjectGroup7/calculate.sh
+++ MiniProjectGroup7/calculate.sh
@@ -4,6 +4,6 @@ for i in {2008..2021}; do
 	sameyear=$(grep $i sortedDifferences.txt | awk -F ":" 'BEGIN {samecount=0} {if($2==0) samecount++} END {print samecount}');
 	total=$(grep $i sortedDifferences.txt | wc -l);	
 	dif=$((total-sameyear));
-	echo $i:$sameyear:$dif;
+	echo $i,$sameyear,$dif;
 done
 
