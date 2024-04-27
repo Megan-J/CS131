d1a63a30269d70f24994814484dafbf16e6077ed
Megan <megansp24@sjsu.pic2.isv.ihost.com>
2024-04-26 23:28:20 -0400

    final

--- Worksheet5/script.sh
+++ Worksheet5/script.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-awk -F, '{ if($8 == "United States") print}' 'Global YouTube Statistics.csv' > UnitedStatesData.txt;
+IFS=$'\r\n'
+lines=$(awk -F "," '{ if($8 == "United States") print}' 'Global YouTube Statistics.csv')
+
+for i in $lines; do
+	category=$(echo "$i" | awk -F "," '{print $5}')
+	if [[ $category == "Music" ]]; then
+    		echo $i >> UnitedStates/Music.txt;
+	elif [[ $category == "Entertainment" ]]; then
+    		echo $i >> UnitedStates/Entertainment.txt;
+	elif [[ $category == "Gaming" ]]; then
+    		echo $i >> UnitedStates/Gaming.txt;
+	elif [[ $category == "Comedy" ]]; then
+    		echo $i >> UnitedStates/Comedy.txt;
+	fi
+
+done
 
 categories=("Music" "Entertainment" "Gaming" "Comedy");
 
 for ((i = 0; i < ${#categories[@]}; i++)) ; do
 	category_name="${categories[i]}";
-	grep "^[^,]*,[^,]*,[^,]*,[^,]*,"$category_name",*" 'UnitedStatesData.txt' > UnitedStates/${category_name}.txt;
 	echo "Number of Entries in Category: ${category_name}" >> ws5.txt;
 	wc -l UnitedStates/${category_name}.txt >> ws5.txt;
 done
