f2ec3e021c8a72ef1eb93362239b22dd120548d9
Megan-J <mmju2103@gmail.com>
2024-04-30 21:45:22 -0700

    deleted extra files

--- MiniProjectGroup7/calculate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in {2008..2021}; do 
-	sameyear=$(grep $i sortedDifferences.txt | awk -F ":" 'BEGIN {samecount=0} {if($2==0) samecount++} END {print samecount}');
-	total=$(grep $i sortedDifferences.txt | wc -l);	
-	dif=$((total-sameyear));
-	echo $i,$sameyear,$dif;
-done
-
